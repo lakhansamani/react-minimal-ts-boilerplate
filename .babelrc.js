ac6a1c51c55713fc5997161869d48ed6f7fe959e
lakhansamani <lakhan.m.samani@gmail.com>
2020-07-24 17:20:04 +0530

    fix indentation

--- .babelrc.js
+++ .babelrc.js
@@ -1,25 +1,25 @@
 const presets = [
-	'@babel/preset-react',
-	'@babel/preset-typescript',
-	[
-		'@babel/env',
-		{
-			targets: {
-				edge: '17',
-				firefox: '60',
-				chrome: '67',
-				safari: '11.1',
-			},
-			corejs: 2,
-		},
-	],
+  '@babel/preset-react',
+  '@babel/preset-typescript',
+  [
+    '@babel/env',
+    {
+      targets: {
+        edge: '17',
+        firefox: '60',
+        chrome: '67',
+        safari: '11.1',
+      },
+      corejs: 2,
+    },
+  ],
 ];
 
 const plugins = [
-	'@babel/plugin-transform-spread',
-	'@babel/plugin-proposal-object-rest-spread',
-	'@babel/plugin-syntax-dynamic-import',
-	'@babel/proposal-class-properties',
+  '@babel/plugin-transform-spread',
+  '@babel/plugin-proposal-object-rest-spread',
+  '@babel/plugin-syntax-dynamic-import',
+  '@babel/proposal-class-properties',
 ];
 
 module.exports = { presets, plugins };
--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,10 @@
 module.exports = {
-	roots: ['<rootDir>/src'],
-	transform: {
-		'\\.(ts|tsx)?$': 'babel-jest',
-	},
-	testMatch: ['<rootDir>/src/**/?(*.)test.{ts,tsx}'], // looks for your test
-	moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-	testPathIgnorePatterns: ['/node_modules/', '/public/'],
-	setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
+  roots: ['<rootDir>/src'],
+  transform: {
+    '\\.(ts|tsx)?$': 'babel-jest',
+  },
+  testMatch: ['<rootDir>/src/**/?(*.)test.{ts,tsx}'], // looks for your test
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  testPathIgnorePatterns: ['/node_modules/', '/public/'],
+  setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
 };
