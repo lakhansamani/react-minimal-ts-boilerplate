783e53930a9a8d52d7561dfcd541507bc456bd5a
Lakhan Samani <lakhan.m.samani@gmail.com>
2020-11-02 14:53:36 +0530

    feat: add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,35 @@
-import React from 'react';
+import React, { Suspense, lazy } from 'react';
+import { BrowserRouter, Link, Switch, Route } from 'react-router-dom';
 
-const App = () => (
-  <div>
+const Home = lazy(() => import('./pages/Home'));
+const About = lazy(() => import('./pages/About'));
+const PageNotFound = lazy(() => import('./pages/404'));
+
+const App: React.FC = () => (
+  <BrowserRouter>
     <h1>Welcome to react app</h1>
-  </div>
+    <ul>
+      <li>
+        <Link to="/">Home</Link>
+      </li>
+      <li>
+        <Link to="/about">About</Link>
+      </li>
+    </ul>
+    <Suspense fallback={<div>Loading...</div>}>
+      <Switch>
+        <Route path="/" exact>
+          <Home />
+        </Route>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="*">
+          <PageNotFound />
+        </Route>
+      </Switch>
+    </Suspense>
+  </BrowserRouter>
 );
 
 export default App;
---
+++ src/pages/404.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const PageNotFound = () => {
+  return <h1>404 Page not found!</h1>;
+};
+
+export default PageNotFound;
